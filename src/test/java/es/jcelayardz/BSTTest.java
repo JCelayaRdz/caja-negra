f45174eb9d4edb39a7fd823f3e8143ee5c7c9937
JCelayaRdz <jcelayardz@gmail.com>
2023-12-12 11:01:28 +0100

    se añade prueba parametrizada de valor limite de insert

--- src/test/java/es/jcelayardz/BSTTest.java
+++ src/test/java/es/jcelayardz/BSTTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,85 +27,146 @@ public class BSTTest {
         bst = new BST();
     }
 
-    @Test
-    @DisplayName("test insertar valores con recursive a true")
-    void testInsertRecursirve() throws DepthException {
-        int[] valoresAceptados = {-2500,-2499,1250,2498,2499};
+    static Stream<Arguments> casosPruebaInsertValorLimite() {
+        return Stream.of(
+                Arguments.of(-2501, true, 0),
+                Arguments.of(-2501, true, 1),
+                Arguments.of(-2501, true, 25),
+                Arguments.of(-2501, true, 49),
+                Arguments.of(-2501, true, 50),
+                Arguments.of(-2501, true, 51),
+                Arguments.of(-2501, false, 0),
+                Arguments.of(-2501, false, 1),
+                Arguments.of(-2501, false, 25),
+                Arguments.of(-2501, false, 49),
+                Arguments.of(-2501, false, 50),
+                Arguments.of(-2501, false, 51),
 
-        Exception e1 = assertThrows(IllegalArgumentException.class, () -> {
-            bst.insert(-2501, true);
-        });
-        assertEquals("El dato que intentas insertar está fuera del rango permitido", e1.getMessage());
+                Arguments.of(-2500, true, 0),
+                Arguments.of(-2500, true, 1),
+                Arguments.of(-2500, true, 25),
+                Arguments.of(-2500, true, 49),
+                Arguments.of(-2500, true, 50),
+                Arguments.of(-2500, true, 51),
+                Arguments.of(-2500, false, 0),
+                Arguments.of(-2500, false, 1),
+                Arguments.of(-2500, false, 25),
+                Arguments.of(-2500, false, 49),
+                Arguments.of(-2500, false, 50),
+                Arguments.of(-2500, false, 51),
 
-        for (int i = 0; i < valoresAceptados.length; i++) {
-            bst.insert(valoresAceptados[i], true);
-            assertEquals(i+1,
-                    bst.size(),
-                    String.format("El árbol debería tener %d elementos pero tiene %d.",i+1, bst.size()));
-        }
+                Arguments.of(-2499, true, 1),
+                Arguments.of(-2499, true, 0),
+                Arguments.of(-2499, true, 25),
+                Arguments.of(-2499, true, 49),
+                Arguments.of(-2499, true, 50),
+                Arguments.of(-2499, true, 51),
+                Arguments.of(-2499, false, 0),
+                Arguments.of(-2499, false, 1),
+                Arguments.of(-2499, false, 25),
+                Arguments.of(-2499, false, 49),
+                Arguments.of(-2499, false, 50),
+                Arguments.of(-2499, false, 51),
 
-        Exception e2 = assertThrows(IllegalArgumentException.class, () -> {
-           bst.insert(2500, true);
-        });
-        assertEquals("El dato que intentas insertar está fuera del rango permitido", e2.getMessage());
-    }
+                Arguments.of(0, true, 1),
+                Arguments.of(0, true, 0),
+                Arguments.of(0, true, 25),
+                Arguments.of(0, true, 49),
+                Arguments.of(0, true, 50),
+                Arguments.of(0, true, 51),
+                Arguments.of(0, false, 0),
+                Arguments.of(0, false, 1),
+                Arguments.of(0, false, 25),
+                Arguments.of(0, false, 49),
+                Arguments.of(0, false, 50),
+                Arguments.of(0, false, 51),
 
-    @Test
-    @DisplayName("test insertar valores con recursive a false")
-    void testInsertNotRecursirve() throws DepthException {
-        int[] valoresAceptados = {-2500,-2499,1250,2498,2499};
+                Arguments.of(2499, true, 1),
+                Arguments.of(2499, true, 0),
+                Arguments.of(2499, true, 25),
+                Arguments.of(2499, true, 49),
+                Arguments.of(2499, true, 50),
+                Arguments.of(2499, true, 51),
+                Arguments.of(2499, false, 0),
+                Arguments.of(2499, false, 1),
+                Arguments.of(2499, false, 25),
+                Arguments.of(2499, false, 49),
+                Arguments.of(2499, false, 50),
+                Arguments.of(2499, false, 51),
 
-        Exception e1 = assertThrows(IllegalArgumentException.class, () -> {
-            bst.insert(-2501, false);
-        });
-        assertEquals("El dato que intentas insertar está fuera del rango permitido", e1.getMessage());
+                Arguments.of(2500, true, 1),
+                Arguments.of(2500, true, 0),
+                Arguments.of(2500, true, 25),
+                Arguments.of(2500, true, 49),
+                Arguments.of(2500, true, 50),
+                Arguments.of(2500, true, 51),
+                Arguments.of(2500, false, 0),
+                Arguments.of(2500, false, 1),
+                Arguments.of(2500, false, 25),
+                Arguments.of(2500, false, 49),
+                Arguments.of(2500, false, 50),
+                Arguments.of(2500, false, 51),
 
-        for (int i = 0; i < valoresAceptados.length; i++) {
-            bst.insert(valoresAceptados[i], false);
-            assertEquals(i+1,
-                    bst.size(),
-                    String.format("El árbol debería tener %d elementos pero tiene %d.",i+1, bst.size()));
-        }
+                Arguments.of(2501, true, 0),
+                Arguments.of(2501, true, 1),
+                Arguments.of(2501, true, 25),
+                Arguments.of(2501, true, 49),
+                Arguments.of(2501, true, 50),
+                Arguments.of(2501, true, 51),
+                Arguments.of(2501, false, 0),
+                Arguments.of(2501, false, 1),
+                Arguments.of(2501, false, 25),
+                Arguments.of(2501, false, 49),
+                Arguments.of(2501, false, 50),
+                Arguments.of(2501, false, 51)
 
-        Exception e2 = assertThrows(IllegalArgumentException.class, () -> {
-            bst.insert(2500, false);
-        });
-        assertEquals("El dato que intentas insertar está fuera del rango permitido", e2.getMessage());
-    }
-
-    @Test
-    @DisplayName("test insertar mas de 50 valores de forma recursiva")
-    void testInsertar50ValoresRecursivo() {
-        Random rand = new Random();
-
-        Exception e = assertThrows(DepthException.class, () -> {
-        for (int i = 0; i <= 50; i++) {
-            bst.insert(i, true);
-        }});
-
-        System.out.println(e.getMessage());
-
-        assertEquals(
-                "Intentas insertar en la altura 51, la máxima permitida es 50",
-                e.getMessage()
         );
     }
 
-    @Test
-    @DisplayName("test insertar mas de 50 valores de forma no recursiva")
-    void testInsertar50ValoresNoRecursivo() {
-        Random rand = new Random();
+    @ParameterizedTest
+    @MethodSource("casosPruebaInsertValorLimite")
+    void testInsertValorLimite(int valor, boolean recursive, int profundidad) throws DepthException {
+        // Primero insertamos tantos valores como sea necesario teniendo en cuenta
+        // el parametro profundidad
+        insertarValores(bst, profundidad, recursive);
 
-        Exception e = assertThrows(DepthException.class, () -> {
-            for (int i = 0; i <= 50; i++) {
-                bst.insert(i, false);
-            }});
-
-        System.out.println(e.getMessage());
-
-        assertEquals(
-                "Intentas insertar en la altura 51, la máxima permitida es 50",
-                e.getMessage()
-        );
+        if (valor < -2500 || valor > 2499) {
+            assertThrows(IllegalArgumentException.class, () -> {
+                bst.insert(valor, recursive);
+            });
+        } else if (profundidad > 50) {
+            assertThrows(DepthException.class, () -> {
+                bst.insert(valor, recursive);
+            });
+        } else {
+            assertDoesNotThrow(() -> {
+                bst.insert(valor, recursive);
+            });
+        }
     }
+
+    void insertarValores(BST bst, int numAInsertar, boolean recursirve) throws DepthException {
+        // Generamos tantos numeros como profundidad deba tener el arbol
+        for (int i=0; i < numAInsertar; i++) {
+            int numeroRandom;
+
+            // Iteramos hasta encontrar un numero que NO este repetido en el arbol
+            while (true) {
+                numeroRandom = generarNumeroValido();
+                if (bst.search(numeroRandom) == null) {
+                    break;
+                }
+            }
+
+            bst.insert(numeroRandom, recursirve);
+        }
+    }
+
+    int generarNumeroValido() {
+        Random random = new Random();
+        // genera un número aleatorio en el rango de 0 a 4998,
+        // luego le restamos 2500 para obtener el rango deseado de -2500 a 2499
+        return random.nextInt(4999) - 2500;
+    }
+
 }
